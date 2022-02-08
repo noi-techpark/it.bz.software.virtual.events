3864ddd3db0fed93e4ac3005985b98067149f693
gweteschnik <karl.weteschnik@ebcont.com>
2022-02-08 15:41:51 +0100

    added s3 backend example, some tf fixes

--- main.tf
+++ main.tf
@@ -6,14 +6,7 @@ terraform {
     }
   }
 
-  #backend "s3" {
-  #  bucket         = "<BUCKET_NAME>"
-  #  key            = "ecs-tf/state/terraform.tfstate"
-  #  region         = "eu-central-1"
-  #  #encrypt        = true
-  #  #kms_key_id     = "alias/terraform-bucket-key"
-  #  dynamodb_table = "terraform-state"
-  #}
+  #backend "s3" {}
 }
 
 provider "aws" {
